0729eb814ac25520103057e56d7c9ebb89e574e2
Ana Laura <anarocha_2412@outlook.com>
2025-11-28 17:23:10 -0300

    modificações simples

--- src/components/explorer/index.tsx
+++ src/components/explorer/index.tsx
@@ -6,49 +6,65 @@ import { useState } from "react";
 
 
 const RenderExplorer = () =>  {
-
+    const {width, } = Dimensions.get("window");
     const[checkIn, setCheckIn] = useState("");
     const[checkOut, setCheckOut] = useState("");
     const[calendar, setCalendar] = useState < "checkin" | "checkout" >();
 
     return (
         <AuthContainer>
-        {/*children*/}
-            <View>
-                <TouchableOpacity onPress={() => setCalendar("checkin")}>
-                    <TextField
-                        label="Check-in"
-                        icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
-                        placeholder="selecione uma data"
-                        value= {checkIn}
-                    />
-                </TouchableOpacity>
-                {/*<DateSelector/> */}
-                    
-                {calendar === "checkin" && (
-                    <DateSelector
-                    onSelectDate={(date) =>
-                        setCheckIn(date)
-                    }/>
-                )}
+            {/*children*/}
+            
+            <View 
+                style={{
+                display: "flex", 
+                flexDirection: "row",
+                gap: width * 0.05,
+                justifyContent: "center"
+                }}>
 
-                <TouchableOpacity onPress={() => setCalendar("checkout")}>
-                    <TextField
-                        label="Check-out"
-                        icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
-                        placeholder="selecione uma data"
-                        value={checkOut}
-                    />
-                </TouchableOpacity>
+                <View style= {{display: "flex",flexDirection: "column"}}>
+                    <TouchableOpacity onPress={() => setCalendar("checkin")}>
+                        <View style={{width: width * 0.42}}>
+                            <TextField
+                                label="Check-in"
+                                icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
+                                placeholder="selecione uma data"
+                                value= {checkIn}
+                            />
+                        </View>
+                    </TouchableOpacity>
+                    {/*<DateSelector/> */}
+                        
+                    {calendar === "checkin" && (
+                        <DateSelector
+                        onSelectDate={(date) =>
+                            setCheckIn(date)
+                        }/>
+                    )}
+                </View>
 
-                {/*<DateSelector/> */}
-                {calendar === "checkout" && (
-                    <DateSelector
-                    onSelectDate={(date) =>
-                        setCheckOut(date)
-                    }/>
-                )}
+                <View style= {{display: "flex",flexDirection: "column"}}>
+                    <TouchableOpacity onPress={() => setCalendar("checkout")}>
+                        <View style={{width: width * 0.42}}>
+                            <TextField
+                                label="Check-out"
+                                icon={{ lib: "FontAwesome5", name: "calendar-alt" }}
+                                placeholder="selecione uma data"
+                                value={checkOut}
+                            />
+                        </View>
+                    </TouchableOpacity>
 
+                    {/*<DateSelector/> */}
+                    {calendar === "checkout" && (
+                        <DateSelector
+                        onSelectDate={(date) =>
+                            setCheckOut(date)
+                        }/>
+                    )}
+
+                </View>
             </View>
         </AuthContainer>
     );
